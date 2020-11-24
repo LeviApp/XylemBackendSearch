cbbba89d4a77eac9aa276cfec3a8e5be1508bdc2
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2020-11-23 23:56:09 -0700

    saved query data as json

--- src/main/java/xylembackend/search/repo/RecommendedPlantRepo.java
+++ src/main/java/xylembackend/search/repo/RecommendedPlantRepo.java
@@ -18,6 +18,6 @@ public interface RecommendedPlantRepo extends JpaRepository<RecommendedPlant, In
     // @Query(value = "SELECT common_name, plant_id, recommended_to, COUNT(plant_id) FROM RECOMMENDEDPLANT INNER JOIN PLANT ON PLANT.id = RECOMMENDEDPLANT.plant_id AND recommended_to = 1 GROUP BY plant_id, recommended_to ORDER BY plant_id", nativeQuery = true)
     // public List<Plant> orderByLikesDesc(Boolean bool);
 
-    @Query(value = "SELECT PLANT.id, PLANT.user_id, PLANT.common_name, PLANT.scientific_name, PLANT.family_common_name, PLANT.family, PLANT.genus, PLANT.year, PLANT.image_url, COUNT(plant_id) AS total_recommends, recommended_to FROM RECOMMENDEDPLANT INNER JOIN PLANT ON PLANT.id = RECOMMENDEDPLANT.plant_id AND recommended_to = ?1 GROUP BY plant_id, recommended_to ORDER BY PLANT.id", nativeQuery = true)
+    @Query(value = "SELECT JSON_OBJECT('id', PLANT.id, 'user_id', PLANT.user_id, 'common_name',  PLANT.common_name, 'scientific_name', PLANT.scientific_name, 'family_common_name', PLANT.family_common_name, 'family', PLANT.family, 'genus', PLANT.genus, 'year', PLANT.year, 'image_url', PLANT.image_url, 'total_recommends', COUNT(RECOMMENDEDPLANT.plant_id), 'recommended_to', RECOMMENDEDPLANT.recommended_to) FROM RECOMMENDEDPLANT INNER JOIN PLANT ON PLANT.id = RECOMMENDEDPLANT.plant_id AND recommended_to = ?1 GROUP BY plant_id, recommended_to ORDER BY PLANT.id", nativeQuery = true)
     public List<Object> listRecommended(String userid);
 }
--- src/main/java/xylembackend/search/service/RecommendedPlantService.java
+++ src/main/java/xylembackend/search/service/RecommendedPlantService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import xylembackend.search.dao.RecommendedPlantDao;
+import xylembackend.search.model.Plant;
 import xylembackend.search.model.RecommendedPlant;
 import xylembackend.search.model.RecommendedPlantData;
 import xylembackend.search.repo.RecommendedPlantRepo;
